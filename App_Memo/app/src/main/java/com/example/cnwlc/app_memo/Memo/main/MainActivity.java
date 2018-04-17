bc649c9f5a2a77f8b0b8ff6db183766fa62dcf89
DESKTOP-0HPCUNM\cnwlc <cnwlcjf1@naver.com>
2018-04-17 23:16:10 +0900

    realm test

--- App_Memo/app/src/main/java/com/example/cnwlc/app_memo/Memo/main/MainActivity.java
+++ App_Memo/app/src/main/java/com/example/cnwlc/app_memo/Memo/main/MainActivity.java
@@ -1,23 +1,25 @@
 package com.example.cnwlc.app_memo.Memo.main;
 
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.example.cnwlc.app_memo.Common.BaseActivity;
-import com.example.cnwlc.app_memo.Memo.Item;
-import com.example.cnwlc.app_memo.Memo.RecyclerAdapter;
 import com.example.cnwlc.app_memo.R;
+import com.example.cnwlc.app_memo.UserDB;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends BaseActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @BindView(R.id.mainA_recycler_view)
     RecyclerView recyclerView;
+
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +34,39 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initView() {
-//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-//        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setLayoutManager(linearLayoutManager);
+        Realm.init(this);
+        realm = Realm.getDefaultInstance();
+        RealmResults<UserDB> userList = getUserList();
+        Log.i(TAG, ">>>> userList.size : "+userList.size()); // :0
 
-        List<Item> items = new ArrayList<Item>();
-        for(int i=0; i<20; i++) {
-            Item item = new Item("어느", "멋진", "날");
-            items.add(item);
-        }
+        //유저 정보 데이터 DB 저장
+//        insertUserData();
+        Log.i(TAG, ">>>> userList.size : "+userList.size()); // :1
 
-        recyclerView.setAdapter(new RecyclerAdapter());
+        //유저 정보 삭제
+        deleteuserData();
+        Log.i(TAG, ">>>>>   userList.size :  " + userList.size()); // :0
+    }
+
+    // 유저 정보 데이터 리스트 반환
+    private RealmResults<UserDB> getUserList() {
+        return realm.where(UserDB.class).findAll();
+    }
+
+    // 유저 정보 데이터 DB 저장
+    private void insertUserData() {
+        realm.beginTransaction();
+        UserDB user = realm.createObject(UserDB.class, "Choo");
+        user.setAge(29);
+        realm.commitTransaction();
+    }
+
+    // 유저 정보 삭제
+    private void deleteuserData(){
+        realm.beginTransaction();
+
+        RealmResults<UserDB> userList = realm.where(UserDB.class).findAll();
+        userList.remove(0);
+        realm.commitTransaction();
     }
 }
