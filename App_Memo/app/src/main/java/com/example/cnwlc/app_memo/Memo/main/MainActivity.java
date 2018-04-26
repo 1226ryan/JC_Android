16b6d63d63150567d18394774e8aae4e2d55d10b
DESKTOP-0HPCUNM\cnwlc <cnwlcjf1@naver.com>
2018-04-26 21:53:08 +0900

    SMS 보내기 기능 추가

--- App_Memo/app/src/main/java/com/example/cnwlc/app_memo/Memo/main/MainActivity.java
+++ App_Memo/app/src/main/java/com/example/cnwlc/app_memo/Memo/main/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.cnwlc.app_memo.Memo.main;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.telephony.SmsManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.cnwlc.app_memo.Common.BaseActivity;
 import com.example.cnwlc.app_memo.R;
@@ -34,18 +42,20 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initView() {
-        Realm.init(this);
-        realm = Realm.getDefaultInstance();
-        RealmResults<UserDB> userList = getUserList();
-        Log.i(TAG, ">>>> userList.size : "+userList.size()); // :0
+        sendSMS("01083025038", "hihi");
 
-        //유저 정보 데이터 DB 저장
-//        insertUserData();
-        Log.i(TAG, ">>>> userList.size : "+userList.size()); // :1
-
-        //유저 정보 삭제
-        deleteuserData();
-        Log.i(TAG, ">>>>>   userList.size :  " + userList.size()); // :0
+//        Realm.init(this);
+//        realm = Realm.getDefaultInstance();
+//        RealmResults<UserDB> userList = getUserList();
+//        Log.i(TAG, ">>>> userList.size : "+userList.size()); // :0
+//
+//        //유저 정보 데이터 DB 저장
+////        insertUserData();
+//        Log.i(TAG, ">>>> userList.size : "+userList.size()); // :1
+//
+//        //유저 정보 삭제
+//        deleteuserData();
+//        Log.i(TAG, ">>>>>   userList.size :  " + userList.size()); // :0
     }
 
     // 유저 정보 데이터 리스트 반환
@@ -68,5 +78,56 @@ public class MainActivity extends BaseActivity {
         RealmResults<UserDB> userList = realm.where(UserDB.class).findAll();
         userList.remove(0);
         realm.commitTransaction();
+    }
+
+    private void sendSMS(String phoneNumber, String message) {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+        String strMessage = "인증번호 : " + message;
+
+        // 각각 위에서부터 문자 전송, 문자 수신에 관련하여 sendTextMessage()에 넘겨줄 값들입니다
+        PendingIntent senTPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getApplicationContext(), "SMS 전송", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getApplicationContext(), "Generic failure", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getApplicationContext(), "No service", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getApplicationContext(), "Null PDU", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getApplicationContext(), "Radio off", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getApplicationContext(), "SMS 전송 완료", Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getApplicationContext(), "SMS 전송 실패", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, strMessage, senTPI, deliveredPI);
     }
 }
