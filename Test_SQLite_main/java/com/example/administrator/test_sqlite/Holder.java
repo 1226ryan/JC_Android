48b15e4da4835f554754de78ca57d3f95de0ca37
JiCheolChoo <36270424+JC-Choo@users.noreply.github.com>
2018-05-11 18:39:47 +0900

    4. test_sqlite
    4. test_sqlite

---
+++ Test_SQLite_main/java/com/example/administrator/test_sqlite/Holder.java
@@ -0,0 +1,10 @@
+package com.example.administrator.test_sqlite;
+
+import android.widget.TextView;
+
+public class Holder {
+    public TextView tvId;
+    public TextView tvName;
+    public TextView tvAge;
+    public TextView tvPhone;
+}
